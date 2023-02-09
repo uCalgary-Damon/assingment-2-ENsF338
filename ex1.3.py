c27b72d9c7affbf26b6d406a156866b53d5b1269
uCalgary-Damon <damon.mazurek@ucalgary.ca>
2023-02-09 16:40:29 -0700

    Added ex1.3 and ex1.5

--- ex1.3.py
+++ ex1.3.py
@@ -1,1 +1,11 @@
+# good_fib was produced using ChatGPT
+def good_fib(n, cache={}):
+    if n == 0 or n == 1:
+        return n
+    if n in cache:
+        return cache[n]
+    else:
+        cache[n] = good_fib(n-1, cache) + good_fib(n-2, cache)
+        return cache[n]
+    
 
---
+++ ex1.5.py
@@ -0,0 +1,41 @@
+import timeit
+import matplotlib.pyplot as plt
+
+def bad_fib(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return bad_fib(n-1) + bad_fib(n-2)
+    
+# good_fib was produced using ChatGPT
+def good_fib(n, cache={}):
+    if n == 0 or n == 1:
+        return n
+    if n in cache:
+        return cache[n]
+    else:
+        cache[n] = good_fib(n-1, cache) + good_fib(n-2, cache)
+        return cache[n]
+    
+def main():
+
+    bad_fib_list = []
+    good_fib_list = []
+    n = []
+
+    #Gets timing for fib(0) to fib(35)
+    for i in range(36):
+        bad_fib_list.append(timeit.timeit(lambda: bad_fib(i), number=1))
+        good_fib_list.append(timeit.timeit(lambda: good_fib(i), number=1))
+        n.append(i)
+    
+    #Plots timing data
+    plt.xlabel("nth Fibonacci Number")
+    plt.ylabel("Seconds (s)")
+    plt.scatter(n, bad_fib_list, label="Bad Fib", color="r")
+    plt.scatter(n, good_fib_list, label="Good Fib", color="g")
+    plt.figlegend(loc="upper center")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
